6f063658154d616911cd68f9226b5ee66de3e65c
Edu Aguilar <eduardoaguilartirado@gmail.com>
2025-11-12 20:54:28 +0100

    remove tweet exercise

--- index.js
+++ index.js
@@ -1,22 +1,4 @@
 import { tweetListController } from "./tweet-list/tweet-list.controller.js";
 
-const button = document.querySelector("button");
-
-button.addEventListener("click", () => {
-  const tweetListContainer = document.querySelector("#tweets");
-  tweetListContainer.innerHTML = "";
-  tweetListController(tweetListContainer)
-  // 1- deshabilitar el botón
-  // button.setAttribute("disabled", "");
-  
-  // 2- borrar el botón
-  // button.remove()
-})
-
-document.querySelector("#test").addEventListener("click", () => { console.log("btn1")})
-document.querySelector("#test2").addEventListener("click", () => { console.log("btn2")})
-document.querySelector("#parent").addEventListener("click", (event) => {
-  console.log("parent")
-  event.stopPropagation()
-})
-document.querySelector("body").addEventListener("click", () => { console.log("body")})
+const tweetListContainer = document.querySelector("#tweets");
+tweetListController(tweetListContainer)
