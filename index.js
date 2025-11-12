2c9a359c842a6585ea9a4ae5c3a60532fd6ed200
Edu Aguilar <eduardoaguilartirado@gmail.com>
2025-11-12 20:33:36 +0100

    show tweets when button is clicked

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 import { tweetListController } from "./tweet-list/tweet-list.controller.js";
 
-const tweetListContainer = document.querySelector("#tweets")
-tweetListController(tweetListContainer)
+const button = document.querySelector("button");
+
+button.addEventListener("click", () => {
+  const tweetListContainer = document.querySelector("#tweets");
+  tweetListContainer.innerHTML = "";
+  tweetListController(tweetListContainer)
+  // 1- deshabilitar el botón
+  // button.setAttribute("disabled", "");
+  
+  // 2- borrar el botón
+  // button.remove()
+})
