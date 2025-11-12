2e210114af66ae09d182d240ee3e0512a31f0462
Edu Aguilar <eduardoaguilartirado@gmail.com>
2025-11-12 22:04:53 +0100

    example handling promises with fetch

--- index.js
+++ index.js
@@ -2,3 +2,16 @@ import { tweetListController } from "./tweet-list/tweet-list.controller.js";
 
 const tweetListContainer = document.querySelector("#tweets");
 tweetListController(tweetListContainer)
+
+
+const fetchExample = async () => {
+  // fetch('https://jsonplaceholder.typicode.com/todos')
+  //   .then(response => response.json())
+  //   .then(data => console.log(data));
+
+  const response = await fetch('https://jsonplaceholder.typicode.com/todos');
+  const data = await response.json();
+  console.log(data)
+}
+
+fetchExample();
