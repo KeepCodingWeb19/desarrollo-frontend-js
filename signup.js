089e0c4058e3b0e696c0ad56aae4805dddf1ac8e
Edu Aguilar <eduardoaguilartirado@gmail.com>
2025-11-17 23:01:15 +0100

    show notifications using notification controller

--- signup.js
+++ signup.js
@@ -1,5 +1,12 @@
 import { signupController } from "./signup/signup.controller.js";
+import { notificationsController } from "./notifications/notifications.controller.js"
 
 const signupForm = document.querySelector("form")
+const notificationsContainer = document.querySelector("#notifications")
 
+const {showNotification}  = notificationsController(notificationsContainer)
 signupController(signupForm);
+
+signupForm.addEventListener("signup-validation-error", (event) => {
+    showNotification(event.detail.message, event.detail.type)
+})
--- signup/signup.controller.js
+++ signup/signup.controller.js
@@ -2,6 +2,8 @@ export const signupController = (signupForm) => {
 
   signupForm.addEventListener("submit", (event) => {
     event.preventDefault();
+    // let isFormValid = true;
+    const errors = []
 
     const password = signupForm.querySelector("#password");
     const passwordConfirmation = signupForm.querySelector("#passwordConfirm");
@@ -9,15 +11,35 @@ export const signupController = (signupForm) => {
 
     const emailRegExp = new RegExp(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/);
     if (!emailRegExp.test(email.value)) {
-      alert("ERROR, email incorrecto")
+      // isFormValid = false;
+      const validationErrorEvent = new CustomEvent('signup-validation-error', {
+        detail: {
+          message: "El email no es válido",
+          type: "error"
+        }
+      });
+      errors.push(validationErrorEvent)
     }
 
-    if (password.value === passwordConfirmation.value) {
-      alert("TODO OK")
-    } else {
-      alert("ERROR, passwords diferentes")
+    if (password.value !== passwordConfirmation.value) {
+      // isFormValid = false;
+      const validationErrorEvent = new CustomEvent('signup-validation-error', {
+        detail: {
+          message: "Las contraseñas no coinciden",
+          type: "error"
+        }
+      });
+      errors.push(validationErrorEvent)
     }
 
+    errors.forEach(errorEvent => {
+      signupForm.dispatchEvent(errorEvent)
+    })
+
+    // si no hay errores, llamo al api
+    if (errors.length === 0) {
+      // consumir api
+    }
 
   })
 }
