ae380a156155a279e2a784fb61d80eeccb4fa282
Edu Aguilar <eduardoaguilartirado@gmail.com>
2025-11-17 19:22:44 +0100

    hide notification after 5 segs

--- notifications/notifications.controller.js
+++ notifications/notifications.controller.js
@@ -2,6 +2,9 @@
 export function notificationsController(notificationsContainer) {
   const showNotification = (message) => {
     notificationsContainer.innerHTML = `<h3>${message}</h3>`;
+    setTimeout(() => {
+      notificationsContainer.innerHTML = '';
+    }, 5000);
   }
 
   return {
--- tweet-list/tweet-list.view.js
+++ tweet-list/tweet-list.view.js
@@ -6,5 +6,5 @@ export const buildTweet = (tweet) => {
 }
 
 export const buildEmptyTweets = () => {
-  return `<p>No hay tweets disponibles</p>`
+  return `<img style="width: 400px" src="https://compote.slate.com/images/97d7753a-b5ae-408a-8fed-7ff68b4ce3b7.jpeg" />`
 }
