86f28bedcc87afb495d4b8b4d92fa5999bcf37ec
Edu Aguilar <eduardoaguilartirado@gmail.com>
2025-11-17 21:43:39 +0100

    validate passwords exercise

---
+++ signup/signup.controller.js
@@ -0,0 +1,10 @@
+export const signupController = (signupForm) => {
+
+
+
+    signupForm.addEventListener("submit", (event) => {
+        event.preventDefault();
+        console.log("botón pulsado, toca validar");
+        
+    })
+}
