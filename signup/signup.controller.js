780f4fb85091e09be8432223ead3a4266b3becda
Edu Aguilar <eduardoaguilartirado@gmail.com>
2025-11-17 22:13:43 +0100

    validate email

--- signup/signup.controller.js
+++ signup/signup.controller.js
@@ -1,10 +1,23 @@
 export const signupController = (signupForm) => {
 
+  signupForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const password = signupForm.querySelector("#password");
+    const passwordConfirmation = signupForm.querySelector("#passwordConfirm");
+    const email = signupForm.querySelector("#email");
+
+    const emailRegExp = new RegExp(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/);
+    if (!emailRegExp.test(email.value)) {
+      alert("ERROR, email incorrecto")
+    }
+
+    if (password.value === passwordConfirmation.value) {
+      alert("TODO OK")
+    } else {
+      alert("ERROR, passwords diferentes")
+    }
 
 
-    signupForm.addEventListener("submit", (event) => {
-        event.preventDefault();
-        console.log("botón pulsado, toca validar");
-        
-    })
+  })
 }
