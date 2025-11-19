0ccdccf0b773471ecb2c96688c138c105b7a19ee
Edu Aguilar <eduardoaguilartirado@gmail.com>
2025-11-19 20:31:53 +0100

    add tweet url param validation

--- tweet-detail.js
+++ tweet-detail.js
@@ -5,6 +5,10 @@ const searchParams = new URLSearchParams(urlParams);
 
 const tweetId = searchParams.get("tweetId");
 
+if (!tweetId) {
+    window.location.href = "/"
+}
+
 const h1 = document.querySelector("h1");
 
 h1.textContent = h1.textContent + ` ${tweetId}`;
