5ccb10c41fe55a277931b6e56c128bccc84fb0c2
Edu Aguilar <eduardoaguilartirado@gmail.com>
2025-11-19 22:30:03 +0100

    show remove tweet button logic exercise

--- tweet-detail/tweet-detail.controller.js
+++ tweet-detail/tweet-detail.controller.js
@@ -1,10 +1,15 @@
-import { getTweetDetail } from "./tweet-detail.model.js"
+import { getTweetDetail, getUserData } from "./tweet-detail.model.js"
 import { buildTweetDetail } from "./tweet-detail.view.js"
 
 // user esta imagen para el detalle del tweet si el tweet obtenido por sparrest no tiene imagen.
 // https://demofree.sirv.com/nope-not-here.jpg
 
 export const tweetDetailController = async (tweetDetailContainer, tweetId) => {
+
+    const handleRemoveTweetButton = () => {
+        
+    }
+
 
     try {
         const tweet = await getTweetDetail(tweetId)
@@ -13,5 +18,21 @@ export const tweetDetailController = async (tweetDetailContainer, tweetId) => {
         alert(error)
         window.location.href = '/'
     }
+    
+    try {
+        const userData = await getUserData()
+        handleRemoveTweetButton()        
+    } catch (error) {
+        
+    }
 
+
+
+
+    /**
+     * obtener datos del usuario que ha iniciado sesión
+     * comparar el id de usuario logado con el id de usuario del tweet
+     * si es el mismo, crear un botón de borrado
+     * cuando el botón se pulsa, confirmar acción y borrar el tweet.
+     */
 }
--- tweet-detail/tweet-detail.model.js
+++ tweet-detail/tweet-detail.model.js
@@ -1,3 +1,5 @@
+import { constants } from "../utils/constants.js"
+
 export const getTweetDetail = async (tweetId) => {
     let tweet = null;
 
@@ -13,4 +15,24 @@ export const getTweetDetail = async (tweetId) => {
 
 
   return tweet;
+}
+
+export const getUserData = async () => {
+  let userData = null;
+  const token = localStorage.getItem(constants.tokenKey)
+  try {
+      const response = await fetch(`http://localhost:8000/auth/me`, {
+        headers: {
+          "Authorization": `Bearer ${token}`
+        }
+      });
+      if (!response.ok) {
+        throw new Error();
+      }
+      userData = await response.json();
+    } catch (error) {
+      throw new Error("El usuario no existe")
+    }
+
+    return userData;
 }
